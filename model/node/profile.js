36031ac38c88f44818589558d377838c82a738a6
Michael Rubanov <mrubanov@gmail.com>
2024-01-21 13:24:35 +0200

    TODO's

--- model/node/profile.js
+++ model/node/profile.js
@@ -5,6 +5,8 @@ const { debug } = require("../../lib");
 const addProfile = async (session, profileUri) => {
     //TODO get profile id + prefixit with Profile to create a string
     //TODO ensure that profile id is number
+    //TODO add unique constraint
+    //TODO throw exeption if profile exists ? or handle in composoed function?
     try {
         const result = await session.run(
           `CREATE (${joinedTitle}:Profile {profileUri:${profileUri}}})`
