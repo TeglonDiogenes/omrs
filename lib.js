3bd8ff705a962b51841de98a213830b21b46315f
Michael Rubanov <mrubanov@gmail.com>
2024-01-21 11:18:00 +0200

    setting up driver and function for session initialization

--- lib.js
+++ lib.js
@@ -1,3 +1,14 @@
+const neo4j =  require('neo4j-driver');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const initSession = (uri, user, password) => {
+  const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
+  const session = driver.session()
+  return {driver, session};
+}
+
 const createMovie = (name, initialRating = 0) => ({
   name,
   rating: initialRating,
